03ae03737ce162d8752e61046c8395b828fb0d6b
Nathan Conn <nconn34@gmail.com>
2021-11-10 15:40:43 -0800

    added boldPassage() function to UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -14,9 +14,9 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  //if (text.trim().length === 0){
-  //  return 0;
-  //}
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+    return 0;
+  }
   const wordArray = text.split(" ");
   let wordCount = 0;
   wordArray.forEach(function(element){
@@ -26,4 +26,48 @@ function numberOfOccurrencesInText(word, text) {
   });
   return wordCount;
 }
+
+// UI Logic
+
+function boldPassage(word, text) {
+    let htmlString ="<p>";
+    let textArray = text.split(" ");
+    textArray.forEach(function(element) {
+      if (word === element) {
+        htmlString = htmlString.concat("<b>" + element + "</b>");
+      } else {
+        htmlString = htmlString.concat(element);
+      }
+      htmlString = htmlString.concat(" ");
+    });
+    return htmlString + "</p>";
+  }
+
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+    function boldPassage(word, text) {
+  let htmlString ="<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index){
+    if (word === element) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)){
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
+}
+  });
+});
+
 
