a9307cf01237fa5c604e0456c9c2531f4f7cf4e1
Nathan Conn <nconn34@gmail.com>
2021-11-10 19:30:05 -0800

    Fixed code so phrase would bold out

--- js/scripts.js
+++ js/scripts.js
@@ -1,85 +1,69 @@
-//Utility Logic
+// Utility Logic
 
 function noInputtedWord(word, text) {
-  return ((text.trim().length === 0) || word.trim().length === 0);
-}
-
-// Business Logic
-
-function wordCounter(text) {
-  if (text.trim().length === 0) {
-    return 0;
+    return ((text.trim().length === 0) || (word.trim().length === 0));
   }
-  let wordCount = 0;
-  const wordArray = text.split(" ");
-  wordArray.forEach(function(element){
-    if (!Number(element))
-    wordCount++;
-  });
-  return wordCount;
-}
-
-function numberOfOccurrencesInText(word, text) {
-  if (noInputtedWord(word, text)) {
-    return 0;
-  }
-  const wordArray = text.split(" ");
-  let wordCount = 0;
-  wordArray.forEach(function(element){
-    if (element.toLowerCase().includes(word.toLowerCase())) {
-      wordCount++
+  
+  // Business Logic
+  
+  function wordCounter(text) {
+    if (text.trim().length === 0) {
+      return 0;
     }
-  });
-  return wordCount;
-}
-
-// UI Logic
-
-function boldPassage(word, text) {
+    let wordCount = 0;
+    const wordArray = text.split(" ");
+    wordArray.forEach(function(element) {
+      if (!Number(element)) {
+        wordCount++;
+      }
+    });
+    return wordCount;
+  }
+  
+  function numberOfOccurrencesInText(word, text) {
+    if (noInputtedWord(word, text)) {
+      return 0;
+    }
+    const wordArray = text.split(" ");
+    let wordCount = 0;
+    wordArray.forEach(function(element) {
+      if (element.toLowerCase().includes(word.toLowerCase())) {
+        wordCount++;
+      }
+    });
+    return wordCount;
+  }
+  
+  // UI Logic
+  
+  function boldPassage(word, text) {
     if (noInputtedWord(word, text)) {
       return "";
     }
-    let htmlString ="<p>";
+    let htmlString = "<p>";
     let textArray = text.split(" ");
-    textArray.forEach(function(element) {
-      if (word === element) {
+    textArray.forEach(function(element, index) {
+      if (element.toLowerCase().includes(word.toLowerCase())) {
         htmlString = htmlString.concat("<b>" + element + "</b>");
       } else {
         htmlString = htmlString.concat(element);
       }
-      htmlString = htmlString.concat(" ");
+      if (index !== (textArray.length - 1)) {
+        htmlString = htmlString.concat(" ");
+      }
     });
     return htmlString + "</p>";
   }
-
+  
   $(document).ready(function(){
-  $("form#word-counter").submit(function(event){
-    event.preventDefault();
-    const passage = $("#text-passage").val();
-    const word = $("#word").val();
-    const wordCount = wordCounter(passage);
-    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
-    $("#total-count").html(wordCount);
-    $("#selected-count").html(occurrencesOfWord);
-    $("bolded-passage").html(boldPassage(word, passage));
-
-   // ** Ask question: Would it matter if this function was in or out of the document?
-  //  function boldPassage(word, text) {
- // let htmlString ="<p>";
- // let textArray = text.split(" ");
-  //textArray.forEach(function(element, index){
-  //  if (word === element) {
-    //  htmlString = htmlString.concat("<b>" + element + "</b>");
-   // } else {
-   //   htmlString = htmlString.concat(element);
-   // }
-  //  if (index !== (textArray.length - 1)){
-   //   htmlString = htmlString.concat(" ");
-   // }
- // });
- // return htmlString + "</p>";
-// }
+    $("form#word-counter").submit(function(event){
+      event.preventDefault();
+      const passage = $("#text-passage").val();
+      const word = $("#word").val();
+      const wordCount = wordCounter(passage);
+      const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+      $("#total-count").html(wordCount);
+      $("#selected-count").html(occurrencesOfWord);
+      $("#bolded-passage").html(boldPassage(word, passage));
+    });
   });
-});
-
-
